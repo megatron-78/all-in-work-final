603b16b9fd1d5807dffcac86539b448391b2c7a6
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-01-09 21:48:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class Config(object):
     # get a token from @BotFather
     BOT_TOKEN = "6331627404:AAHgjoOQqF4ClxNekEhFibzRVSslt_tsfsk"
     API_ID = "18618422"
-    API_HASH = "6331627404:AAHgjoOQqF4ClxNekEhFibzRVSslt_tsfsk"
+    API_HASH = "f165b1caec3cfa4df943fe1cbe82d22a"
     AUTH_USERS = '6050277919'
 
