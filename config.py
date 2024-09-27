18f95a310b07ff270c9f14bbf1820c011ff9fe46
megatron-78 <mk6454684@gmail.com>
2024-09-27 13:33:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6331627404:AAHgjoOQqF4ClxNekEhFibzRVSslt_tsfsk"
+    BOT_TOKEN = "7447109300:AAG2RVJClcMUIcFPagYMO3z5e3OTqTOyhJw"
     API_ID = "18618422"
     API_HASH = "f165b1caec3cfa4df943fe1cbe82d22a"
-    AUTH_USERS = '6050277919'
+    AUTH_USERS = '6622333718'
 
